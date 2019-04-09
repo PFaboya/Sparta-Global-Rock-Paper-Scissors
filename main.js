9210e44bdadf79fdbde989e726f19d8b1dcb619c
Tech-A08 <tech-a08@macbook-air-3.lan>
2019-04-09 22:18:40 +0100

    able to do one round of rock-paper-scissors

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ if(computerNum <= 1 / 3){
   var computerHand = "scissors";
 }
 
+// winning hand
 if( yourHand == "rock" && computerHand == "scissors"){
   alert("computer has chosen scissors")
   alert("You have brought honour to your family and won one point");
@@ -27,4 +28,36 @@ if( yourHand == "paper" && computerHand == "rock"){
 if( yourHand == "scissors" && computerHand == "paper"){
   alert("computer has chosen paper")
   alert("You have brought honour to your family and won one point");
+}
+
+//losing hand
+if( yourHand == "rock" && computerHand == "paper"){
+  alert("computer has chosen paper")
+  alert("Your life is filled with nothing but shame, beaten by a mere computer");
+}
+
+if( yourHand == "paper" && computerHand == "scissors"){
+  alert("computer has chosen scissors")
+  alert("Your life is filled with nothing but shame, beaten by a mere computer");
+}
+
+if( yourHand == "scissors" && computerHand == "rock"){
+  alert("computer has chosen rock")
+  alert("Your life is filled with nothing but shame, beaten by a mere computer");
+}
+
+//draw
+if( yourHand == "rock" && computerHand == "rock"){
+  alert("computer has also chosen rock")
+  alert("In the heated battle you have both come out equal");
+}
+
+if( yourHand == "paper" && computerHand == "paper"){
+  alert("computer has also chosen paper")
+  alert("In the heated battle you have both come out equal");
+}
+
+if( yourHand == "scissors" && computerHand == "scissors"){
+  alert("computer has also chosen scissors")
+  alert("In the heated battle you have both come out equal");
 }
